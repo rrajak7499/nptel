ccf14a9d390774089d69f3463dc1c79f068592d0
Rohit <rrajak7499@gmail.com>
2022-06-06 17:34:27 +0530

    first commit

---
+++ pages/course/[subject].js
@@ -0,0 +1,73 @@
+import React from "react";
+import { useState } from "react";
+
+function subject() {
+  return (
+    <>
+      <div className="flex flex-row">
+        <div className="side_menu basis-1/4 bg-orange-500">
+          <div className="list-items">
+            <div className="unit">
+              <div className="unit-title">WEEK 1</div>
+              <ul className="list-none">
+                <li className="lession">
+                  <span>Lecture 01 : Introduction</span>
+                </li>
+                <li className="lession">
+                  <span>Lecture 01 : Introduction</span>
+                </li>
+                <li className="lession">
+                  <span>Lecture 01 : Introduction</span>
+                </li>
+                <li className="lession">
+                  <span>Lecture 01 : Introduction</span>
+                </li>
+                <li className="lession">
+                  <span>Lecture 01 : Introduction</span>
+                </li>
+                <li className="lession">
+                  <span>Lecture 01 : Introduction</span>
+                </li>
+              </ul>
+            </div>
+            <div className="unit">
+              <div className="unit-title">WEEK 2</div>
+              <ul className="list-none">
+                <li className="lession">
+                  <span>Lecture 02 : Introduction</span>
+                </li>
+                <li className="lession">
+                  <span>Lecture 02 : Introduction</span>
+                </li>
+                <li className="lession">
+                  <span>Lecture 02 : Introduction</span>
+                </li>
+                <li className="lession">
+                  <span>Lecture 02 : Introduction</span>
+                </li>
+                <li className="lession">
+                  <span>Lecture 02 : Introduction</span>
+                </li>
+                <li className="lession">
+                  <span>Lecture 02 : Introduction</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="main basis-3/4 bg-red-500">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, vero
+            ad consectetur officia voluptatem dolores, sapiente asperiores harum
+            veritatis dolorem fugiat minus. Suscipit, esse corporis nulla
+            doloribus ea ducimus doloremque fuga voluptates, soluta fugit
+            labore, voluptate totam magni minima. Tenetur facilis sit, iusto vel
+            quas sint molestiae laborum incidunt iure.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default subject;
